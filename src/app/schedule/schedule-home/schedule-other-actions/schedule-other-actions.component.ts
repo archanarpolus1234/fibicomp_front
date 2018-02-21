bfee3f01b8a7d8582d14987fa136255c2609c078
Anu Joseph <anu.joseph@polussoftware.com>
2018-02-21 19:02:18 +0530

    Added Changes in Schedule configurations

---
+++ src/app/schedule/schedule-home/schedule-other-actions/schedule-other-actions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-schedule-other-actions',
+  templateUrl: './schedule-other-actions.component.html',
+  styleUrls: ['../../../../assets/css/bootstrap.min.css', '../../../../assets/css/font-awesome.min.css', '../../../../assets/css/style.css', '../../../../assets/css/search.css']
+})
+export class ScheduleOtherActionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
