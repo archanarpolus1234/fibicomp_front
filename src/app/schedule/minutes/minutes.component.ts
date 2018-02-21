84c2e5092c8e84a5da61c06600d004e87cc269b7
Shaji P <shaji.p@polussoftware.com>
2018-02-21 19:22:15 +0530

    Modified changes in Minutes

--- src/app/schedule/minutes/minutes.component.ts
+++ src/app/schedule/minutes/minutes.component.ts
@@ -6,12 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../../../assets/css/bootstrap.min.css', '../../../assets/css/font-awesome.min.css', '../../../assets/css/style.css', '../../../assets/css/search.css']
 })
 export class MinutesComponent implements OnInit {
-  addMinutesEntry: boolean = false;
+  isMinuteEntryPoppedUp: boolean = false;
+  optionSelectedEntityType: string = 'adverseEvents';
   constructor() { }
 
   ngOnInit() {
   }
-  addMinutes($event){
-      this.addMinutesEntry = !this.addMinutesEntry;
+  
+  showAddMinutes($event){
+      if(this.isMinuteEntryPoppedUp == false) {
+          this.isMinuteEntryPoppedUp = true;
+      }
+      this.optionSelectedEntityType = "adverseEvents";
+  }
+  
+  saveMinutes() {
+      
+  }
+  
+  saveAndNew() {
+      this.saveMinutes();
+  }
+  
+  saveAndClose() {
+      this.saveMinutes();
+      if(this.isMinuteEntryPoppedUp == true) {
+          this.isMinuteEntryPoppedUp = false;
+      }
   }
 }
