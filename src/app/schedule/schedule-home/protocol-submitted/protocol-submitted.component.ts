98b40851396b42c7a879801fb57e40a32dd43f52
Farsana Khadar <farsana.khadar@polussoftware.com>
2018-02-22 19:26:31 +0530

    Updated submitted protocols and schedule home

--- src/app/schedule/schedule-home/protocol-submitted/protocol-submitted.component.ts
+++ src/app/schedule/schedule-home/protocol-submitted/protocol-submitted.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ScheduleConfigurationService } from "../../../common/schedule-configuration.service";
+import { Constants } from "../../../constants/constants.service";
 
-@Component({
-  selector: 'app-protocol-submitted',
-  templateUrl: './protocol-submitted.component.html',
-  styleUrls: ['../../../../assets/css/bootstrap.min.css', '../../../../assets/css/font-awesome.min.css', '../../../../assets/css/style.css', '../../../../assets/css/search.css']
-})
+@Component( {
+    selector: 'app-protocol-submitted',
+    templateUrl: './protocol-submitted.component.html',
+    styleUrls: ['../../../../assets/css/bootstrap.min.css', '../../../../assets/css/font-awesome.min.css', '../../../../assets/css/style.css', '../../../../assets/css/search.css']
+} )
 export class ProtocolSubmittedComponent implements OnInit {
+    public result: any = {};
+    outputPath: string;
+    userName: string;
+    constructor( private scheduleConf: ScheduleConfigurationService, private consts: Constants ) { }
 
-  constructor() { }
+    ngOnInit() {
+        this.scheduleConf.currentScheduleData.subscribe( data => {
+            this.result = data;
+            this.outputPath = this.consts.outputPath;
+            this.userName = localStorage.getItem( 'currentUser' );
+        } );
 
-  ngOnInit() {
-      
-      
-  }
-
+    }
 }
