1cfc1ebb136f55d22d7f59c32a4a1d5cebda1de5
Shaji P <shaji.p@polussoftware.com>
2018-04-24 22:41:23 +0530

    updated changes in create proposal form

--- src/app/constants/constants.service.ts
+++ src/app/constants/constants.service.ts
@@ -4,13 +4,13 @@ import { Injectable } from '@angular/core';
 export class Constants {
     //baseUrl : string = "http://192.168.1.76";
     //baseUrl : string = "http://52.20.110.7";
-    baseUrl : string = "http://14.140.179.70";
+    //baseUrl : string = "http://14.140.179.70";
     // baseUrl : string = "http://demo.fibiweb.com/fibi-comp";
-  // baseUrl: string = "";
+   baseUrl: string = "";
 
    // portAndPath: string = ":8080/fibi-comp";
-   portAndPath: string = ":1550/fibi-comp";
-   // portAndPath: string = "";
+   //portAndPath: string = ":1550/fibi-comp";
+    portAndPath: string = "";
 
     expandedSummaryViewUrl: string;
     expandedDonutViewUrl: string;
@@ -82,6 +82,10 @@ export class Constants {
     createGrantCalls: string;
     createProposalUrl: string;
     deleteProposalSponsor: string;
+    deleteProposalPersonUrl: string;
+    deleteBudgetUrl: string;
+    deleteProposalKeywordUrl: string;
+    fetchCostElementUrl: string;
 
     constructor() {
         this.index_url = 'http://192.168.1.76:9200/';
@@ -216,6 +220,14 @@ export class Constants {
         this.deleteIrbProtocol = this.baseUrl + this.portAndPath + '/deleteIrbProtocol';
 
         this.deleteProposalSponsor = this.baseUrl + this.portAndPath + '/deleteProposalSponsor';
+
+        this.deleteProposalPersonUrl  = this.baseUrl+this.portAndPath+'/deleteProposalPerson';
+
+        this.deleteBudgetUrl  = this.baseUrl+this.portAndPath+'/deleteProposalBudget';
+
+        this.deleteProposalKeywordUrl  = this.baseUrl+this.portAndPath+'/deleteProposalKeyword';
+
+        this.fetchCostElementUrl  = this.baseUrl+this.portAndPath+'/fetchCostElementByBudgetCategory';
 
         //this.outputPath = 'http://192.168.1.242:8080/kc-dev';
         //this.outputPath = 'http://demo.fibiweb.com/kc-dev';
--- src/app/proposal/proposal.module.ts
+++ src/app/proposal/proposal.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterModule} from '@angular/router';
-
 import { ProposalComponent } from './proposal.component';
+import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Ng2CompleterModule } from 'ng2-completer';
+import { FileDropModule } from 'ngx-file-drop';
 
-const routes = [{path: '', component: ProposalComponent},
-                {path: 'createProsal', component: ProposalComponent}];
+import { CommitteeMemberNonEmployeeElasticService  } from '../elastic-search/committee-members-nonEmployee-elastic-search.service';
+import { CommitteeMemberEmployeeElasticService } from '../elastic-search/committee-members-employees-elastic-search.service';
+import { GrantService } from "../grant/grant.service";
+import { ProposalCreateEditService } from '../proposal/proposal-create-view.service';
+
+let routes = [{ path: '', component: ProposalComponent},
+              { path: 'proposal', component: ProposalComponent},
+              {path:'createProposal', component: ProposalComponent }];
+
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule,
+    FormsModule, ReactiveFormsModule,
+    Ng2CompleterModule,
+	FileDropModule,
+    RouterModule.forChild( routes )
   ],
-  declarations: [ProposalComponent]
+  declarations: [ProposalComponent],
+  providers: [ProposalCreateEditService,CommitteeMemberEmployeeElasticService,CommitteeMemberNonEmployeeElasticService, GrantService]
 })
 export class ProposalModule { }
